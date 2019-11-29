2a5a1853cc719b627e80d1a85aaa29e0d9dac1f2
Erwan <rr.1.riou@gmail.com>
2019-11-29 02:16:00 +0100

    [Stop] - Stoping the test here as didnt found any way of fetching or extract user data from the callback that i could localstorage or import in redux store...

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,6 @@
+import axios from "axios"
+import { USER_LOGIN, USER_LOGOUT } from "./types"
+
+export login = () => async dispatch => {
+  // SOMEHOW understund how to get fetch the data from the AWS URI
+}
